897f4f279cdeced8f7ae0923a6d3bac9603849a2
raufurislam <raufurislam@gmail.com>
2025-05-07 16:44:30 +0600

    Initialize

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+class Product {
+  id: number;
+  name: string;
+  price: number;
+  quantity: number;
+
+  constructor(id: number, name: string, price: number, quantity: number) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.quantity = quantity;
+  }
+
+  getDetails() {
+    return `Product ${this.name} -$${this.price} stock: ${this.quantity}`;
+  }
+}
