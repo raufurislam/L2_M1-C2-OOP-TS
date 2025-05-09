77ec00745caab1cd8e38da5b9cdf77a67133c98a
raufurislam <raufurislam@gmail.com>
2025-05-09 22:23:27 +0600

    Inherit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
+import { DigitalProduct } from "./models/DigitalProduct";
 import { Product } from "./models/Product";
 
-const sampleProduct = new Product(1, "product-1", 20, 50);
+const sampleProduct = new DigitalProduct(1, "Demo Product", 20, 50, 100);
 
 console.log(sampleProduct.getDetails());
+console.log(sampleProduct.getStockValue());
+console.log(sampleProduct.getDownloadInfo());
--- src/models/DigitalProduct.ts
+++ src/models/DigitalProduct.ts
@@ -1,3 +1,18 @@
 import { Product } from "./Product";
 
-class DigitalProduct extends Product {}
+export class DigitalProduct extends Product {
+  //   downloadSizeInMB: number;
+  constructor(
+    id: number,
+    name: string,
+    price: number,
+    quantity: number,
+    public downloadSizeInMB: number
+  ) {
+    super(id, name, price, quantity);
+    // this.downloadSizeInMB = downloadSizeInMB;
+  }
+  getDownloadInfo() {
+    return `${this.name} is downloading... Size: ${this.downloadSizeInMB}`;
+  }
+}
