44f70d13c2fbd6f854829f34580d18577a91197a
raufurislam <raufurislam@gmail.com>
2025-05-09 21:56:42 +0600

    Follow up

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { Product } from "./models/Product";
 
-const sampleProduct = new Product(1, "demo product", 30, 50);
+const sampleProduct = new Product(1, "product-1", 20, 50);
 
 console.log(sampleProduct.getDetails());
---
+++ src/models/DigitalProduct.ts
@@ -0,0 +1,3 @@
+import { Product } from "./Product";
+
+class DigitalProduct extends Product {}
