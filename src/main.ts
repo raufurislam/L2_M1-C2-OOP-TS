086ca1522ce0a626bfb44677bb24d3722df5d444
raufurislam <raufurislam@gmail.com>
2025-05-12 15:36:53 +0600

    Polymorphism

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { DigitalProduct } from "./models/DigitalProduct";
+import { PhysicalProduct } from "./models/PhysicalProduct";
 import { Product } from "./models/Product";
 
-const sampleProduct = new Product(1, "Demo Product", 20, 50, 100);
+// const sampleProduct = new DigitalProduct(1, "Demo Product", 20, 50, 100);
+const sampleProduct = new Product(1, "Demo Product", 20, 50);
+const sampleProduct1 = new PhysicalProduct(1, "Demo Product", 20, 50);
+// const sampleProduct = new Product(1, "Demo Product", 20, 50);
 
-console.log(sampleProduct.getDetails());
+// console.log(sampleProduct.getDetails());
 // console.log(sampleProduct.getStockValue());
 // console.log(sampleProduct.getDownloadInfo());
-console.log(sampleProduct.sell(210));
+// console.log(sampleProduct.sell(210));
+// console.log(sampleProduct.getDetails());
+
 console.log(sampleProduct.getDetails());
+console.log(sampleProduct.getStockValue());
+console.log(sampleProduct1.getStockValue());
---
+++ src/models/PhysicalProduct.ts
@@ -0,0 +1,8 @@
+import { Purchasable } from "../interfaces/PolymorphismInterface";
+import { Product } from "./Product";
+
+export class PhysicalProduct extends Product implements Purchasable {
+  getStockValue(): number {
+    return this.price;
+  }
+}
